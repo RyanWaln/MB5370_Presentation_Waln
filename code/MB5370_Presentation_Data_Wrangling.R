2e08952cc6878ec0cbea50b44f47ddc2e8eb069a
Ryan Waln <ryan.waln@aol.com>
2026-06-19 13:14:59 +1000

    Created first coding files

---
+++ code/MB5370_Presentation_Data_Wrangling.R
@@ -0,0 +1,24 @@
+---
+  #title: "MB5370 Presentation Waln"
+  #author: "Ryan Waln"
+  #date: "`r format(Sys.time(), '%d %B, %Y')`"
+  #output: html_document
+  ---
+  ## Introduction
+  
+  # This file is meant to format the AIMs coral data for the class project 
+  
+  # Houskeeping
+  rm(list=ls())
+objects()  
+
+
+## Rules for tidy data:
+
+#   Each variable must have its own column.
+
+#   Each observation must have its own row.
+
+#   Each value must have its own cell.
+
+#   ALWAYS: Put each dataset in a tibble and each variable in a column.
